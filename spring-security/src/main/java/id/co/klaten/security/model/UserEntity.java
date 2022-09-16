fdf20051d058df25ee96ec43b87dde61184142e7
dino.prasetyo <dino.prasetyo@gmail.com>
2022-09-16 09:13:46 +0700

    Spring-security

---
+++ spring-security/src/main/java/id/co/klaten/security/model/UserEntity.java
@@ -0,0 +1,68 @@
+package id.co.klaten.security.model;
+
+
+import javax.persistence.*;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.io.Serializable;
+import java.util.Collection;
+import java.util.LinkedHashSet;
+
+@Entity
+@Table(name="M_USER")
+@Data              
+@AllArgsConstructor
+@NoArgsConstructor 
+public class UserEntity implements UserDetails, Serializable
+{
+
+
+  @Id
+  @Column(name="USER_ID", nullable = false)
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Integer userId;
+
+  @Column(name="USER_NAME")
+  private String userName;
+
+  @Column(name="PASSWORD")
+  private String password;
+  
+  public Collection<? extends GrantedAuthority> getAuthorities()
+  {
+    RoleEntity role_Entity_user = new RoleEntity(1, "ROLE_USER");
+    LinkedHashSet<GrantedAuthority> grantedAuthorities = new LinkedHashSet<GrantedAuthority>();
+    grantedAuthorities.add(role_Entity_user);
+    return grantedAuthorities;
+  }
+  
+  public String getUsername()
+  {
+    return this.userName;
+  }
+  
+  public boolean isAccountNonExpired()
+  {
+    return true;
+  }
+  
+  public boolean isAccountNonLocked()
+  {
+    return true;
+  }
+  
+  public boolean isCredentialsNonExpired()
+  {
+    return true;
+  }
+  
+  public boolean isEnabled()
+  {
+    return true;
+  }
+}
